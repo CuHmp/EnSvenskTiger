6d25491afcda954d42f576efdebe62dcfcda2910
Adam Olsson <adaols80@gmail.com>
2020-04-09 15:27:27 +0200

    Added The map assets

---
+++ Assets/Code/TimeTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTester : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        TimeMaster processor = FindObjectOfType<TimeMaster>();
+        processor.onTick.AddListener(Tick);
+    }
+
+    void Tick() {
+        Debug.Log(TimeMaster.GetTime());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
