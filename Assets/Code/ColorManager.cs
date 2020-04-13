4d4ca422a38160ca9b6d9316b0d618c5d11eddfd
Adam Olsson <adaols80@gmail.com>
2020-04-13 15:58:13 +0200

    Added ww2 map with modern outlines

--- Assets/Code/ColorManager.cs
+++ Assets/Code/ColorManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorManager : MonoBehaviour {
+    Hashtable borderChanges = new Hashtable();
+
     public static ColorManager instance;
     private static List<GameObject> countries = null;
 
@@ -14,7 +16,32 @@ public class ColorManager : MonoBehaviour {
             instance = this;
             ColorManager.init();
         }
-        ChangeControl("POL", "GER");
+        borderChanges.Add(new System.DateTime(1939, 9, 17), "POL:GER");
+        borderChanges.Add(new System.DateTime(1939, 3, 15), "CZE:GER");
+        borderChanges.Add(new System.DateTime(1939, 3, 15), "SLO:HUN");
+        borderChanges.Add(new System.DateTime(1940, 7, 21), "LIT:S-RUS");
+        borderChanges.Add(new System.DateTime(1940, 6, 17), "LAT:S-RUS");
+        borderChanges.Add(new System.DateTime(1940, 6, 17), "EST:S-RUS");
+        borderChanges.Add(new System.DateTime(1940, 4, 9), "DEN:GER");
+        borderChanges.Add(new System.DateTime(1940, 4, 15), "NOR:GER");
+        borderChanges.Add(new System.DateTime(1940, 5, 28), "BEL:GER");
+        borderChanges.Add(new System.DateTime(1940, 5, 17), "HOL:GER");
+        borderChanges.Add(new System.DateTime(1940, 6, 14), "FRA:GER");
+        borderChanges.Add(new System.DateTime(1941, 7, 9), "LIT:GER");
+        borderChanges.Add(new System.DateTime(1941, 7, 9), "LAT:GER");
+        borderChanges.Add(new System.DateTime(1941, 9, 1), "EST:GER");
+        borderChanges.Add(new System.DateTime(1941, 9, 1), "S-BEL:GER");
+        borderChanges.Add(new System.DateTime(1941, 9, 26), "S-UKR:GER");
+        borderChanges.Add(new System.DateTime(1941, 9, 1), "S-MOL:ROM");
+
+        borderChanges.Add(new System.DateTime(1944, 4, 30), "S-UKR:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 8, 19), "S-BEL:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 8, 25), "S-MOL:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 8, 30), "ROM:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 12, 31), "LIT:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 12, 31), "LAT:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 12, 31), "EST:S-RUS");
+        borderChanges.Add(new System.DateTime(1944, 12, 31), "HUN:S-RUS");
     }
 
     public static void init() {
@@ -30,7 +57,7 @@ public class ColorManager : MonoBehaviour {
         if(from.ToUpper() == "POL") {
             int OLD_BORDERS_index = FindIndex("W-POL");
             int NULL_index = FindIndex("NULL");
-            instance.StartCoroutine(ChangeColourGradually(OLD_BORDERS_index, NULL_index, 1f));
+            instance.StartCoroutine(ChangeColourGradually(OLD_BORDERS_index, NULL_index, 1.5f));
         }
 
         int target_index = FindIndex(from.ToUpper());
