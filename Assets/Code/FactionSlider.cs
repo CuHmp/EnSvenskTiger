f9278482546f4920e5ec9183ac8ce5bb427dd685
Unknown <fireblaze123@gmail.com>
2020-04-20 16:12:00 +0200

    UI

---
+++ Assets/Code/FactionSlider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactionSlider : MonoBehaviour
+{
+    [SerializeField]
+    Slider OpinionSlider, PowerSlider;
+
+    public void SetOpinion(int opinion)
+    {
+        OpinionSlider.value = opinion;
+    }
+
+    public void SetPower(int power)
+    {
+        PowerSlider.value = power;
+    }
+}
---
+++ Assets/Code/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    FactionSlider Allies, Axis, Soviets;
+
+    [SerializeField]
+    Slider Popularity;
+    void Start()
+    {
+        UpdateStats();
+    }
+
+    
+
+    public void UpdateStats()
+    {
+        Allies.SetOpinion(Player.GetResource(Resource.AlliesOpinion));
+        Allies.SetPower(Player.GetResource(Resource.AlliesPower));
+
+        Axis.SetOpinion(Player.GetResource(Resource.AxisOpinion));
+        Axis.SetPower(Player.GetResource(Resource.AxisPower));
+
+        Soviets.SetOpinion(Player.GetResource(Resource.SovietOpinion));
+        Soviets.SetPower(Player.GetResource(Resource.SovietPower));
+
+        Popularity.value = Player.GetResource(Resource.Popularity);
+
+        // TODO: Iron, money and food
+    }
+}
