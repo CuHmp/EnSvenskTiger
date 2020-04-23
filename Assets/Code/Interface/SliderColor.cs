509c08180539e985983f685bdefaf0d3444864e0
Unknown <fireblaze123@gmail.com>
2020-04-23 14:46:03 +0200

    Slider Color

---
+++ Assets/Code/Interface/SliderColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderColor : MonoBehaviour
+{
+    [SerializeField]
+    Image face;
+    [SerializeField]
+    Slider slider;
+
+    Color green = Color.green, red = Color.red;
+    void FixedUpdate()
+    {
+        face.color = Color.Lerp(red, green, slider.normalizedValue);
+    }
+}
