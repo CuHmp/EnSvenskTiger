9f094c9f74d2d48609662a0b223213f41531d785
Unknown <fireblaze123@gmail.com>
2020-05-08 15:21:33 +0200

    gitflow-feature-stash: Credits

---
+++ Assets/Code/Interface/Credits.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    float accumulator = 0.0f, speed = 50.0f;
+    const float limit = 27.0f;
+
+    [LabeledArray(new string[] {"Coding", "Concept"})]
+    public LangString[] langStrings = new LangString[2];
+    [LabeledArray(new string[] { "Coding", "Concept" })]
+    public Text[] strings = new Text[2];
+
+
+    void Awake()
+    {
+        for (int i = 0; i < langStrings.Length && i < strings.Length; i++)
+        {
+            strings[i].text = langStrings[i].GetText();
+        }
+    }
+    void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * speed;
+        accumulator += Time.deltaTime;
+        if (accumulator > limit || Input.anyKey)
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
